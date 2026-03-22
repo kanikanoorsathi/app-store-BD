186918a0750f45b9972c8ad2105ae0586b7b75f3
kanikanoorsathi <kahinurakter504@gmail.com>
2026-03-22 19:29:58 +0600

    navbar added

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../../assets/logo.png"
 import { VscGithub } from "react-icons/vsc";
+import { Link } from "react-router";
 
 const Navbar = () => {
   return (
@@ -34,56 +35,29 @@ const Navbar = () => {
                 tabIndex="-1"
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
               >
-                <li>
-                  <a>Item 1</a>
-                </li>
-                <li>
-                  <a>Parent</a>
-                  <ul className="p-2">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a>Item 3</a>
-                </li>
+                <li><a>Home</a></li>
+                <li><a>Apps</a></li>
+                <li><a>Installation</a></li>
               </ul>
             </div>
-            <a className="btn btn-ghost text-xl">
-                <img className="w-8" src={logo} alt="logo" />
+            <a className="btn btn-ghost text-xl cursor-pointer text-purple-600">
+                <Link to={'/'}>
+                   <img className="w-8" src={logo} alt="logo" />
+                </Link>
+                
                 App Store BD
                 
             </a>
           </div>
           <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <details>
-                  <summary>Parent</summary>
-                  <ul className="p-2 bg-base-100 w-40 z-1">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              <li><a>Home</a></li>
+              <li><a>Apps</a></li>
+              <li><a>Installation</a></li>
             </ul>
           </div>
           <div className="navbar-end">
-            <a className="btn bg-linear-to-br from-[#632EE3] to-[#9F62F2] transition-all duration-300 cursor-pointer text-white" href=""><VscGithub size={20} />Contribute</a>
+            <a className="btn bg-linear-to-br from-[#632EE3] to-[#9F62F2] transition-all duration-300 cursor-pointer text-white" href="https://github.com/kanikanoorsathi"><VscGithub size={20} />Contribute</a>
           </div>
         </div>
       </div>
